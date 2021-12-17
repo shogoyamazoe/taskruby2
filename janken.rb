ec186a30bf5df4c1e0c6041d28f6b7059d6e09ea
shogoyamaoe <shogo.1227@mineo.jp>
2021-12-18 06:56:19 +0900

    じゃんけん課題2回目

--- janken.rb
+++ janken.rb
@@ -4,21 +4,21 @@ puts "最初はぐー。。。じゃんけん。。。。"
 def janken 
   puts "[0]:グー\n[1]:チョキ\n[2]:パー"
   player_hand = gets.to_i
-  # program_hand = 0
-  program_hand = rand(3)
+  # program_hand = rand(3)
+  program_hand = 0
   
   jankens = ["グー", "チョキ", "パー"]
   puts "あなたの手:#{jankens[player_hand]}, わたしの手:#{jankens[program_hand]}"
       
   if  player_hand == program_hand
     puts "あいこで"
-    return 1
+    return 10
   elsif (player_hand == 0 && program_hand == 1) || (player_hand == 1 && program_hand == 2) || (player_hand == 2 && program_hand == 0)
     puts "じゃんけんはあなたの勝ちです"
-    return 2
+    return 20
   elsif (player_hand == 0 && program_hand == 2) || (player_hand == 1 && program_hand == 0) || (player_hand == 2 && program_hand == 1)
     puts "じゃんけんはあなたの負けです"
-    return 3
+    return 30
   else 
     puts "入力値が異常です"
     puts "数値は0.1.2を入力してください。もう一度入力してください"
@@ -29,18 +29,19 @@ end
 def hoi_win
   puts "[0]:左\n[1]:上\n[2]:右\n[3]:下"
   player = gets.to_i
-  # program = 0
-  program = rand(4)
+  # program = rand(4)
+  program = 0
   
   hoi_win = ["左", "上", "右","下"]
   puts "あなた:#{hoi_win[player]}, わたしの方向:#{hoi_win[program]}"
     
   if player == program
     puts "あなたの勝ち"
+    return 100
   
   elsif player != program
     puts "もう一度じゃんけんしよう！！"
-    return 1
+    return 101
 
   else
     puts "じゃんけんから始めましょう"
@@ -50,8 +51,8 @@ end
 def hoi_lose
   puts "[0]:左\n[1]:上\n[2]:右\n[3]:下"
   player = gets.to_i
-  # program = 0
-  program = rand(4)
+  # program = rand(4)
+  program = 0
 
 
   hoi_lose = ["左", "上", "右","下"]
@@ -59,45 +60,48 @@ def hoi_lose
     
   if player == program
     puts "あなたの負け"
+    return 200
+
   elsif player != program
     puts "もう一度じゃんけんしよう！！"
-    return 1
+    return 201
+
   else 
     puts "じゃんけんから始めましょう"
   end
 end
 
 next_game = janken
-  
-while next_game == 1 do
+
+while next_game == 10 do
   next_game = janken
 end
 
-while next_game == 2 do
-  if hoi_win == 1
-    next_game = janken
+while next_game == 20 do
+  puts "あっち向いてホイをします"
+  puts "あなたは指を指します" 
+  if hoi_win == 101
+    next_game  = janken
+     if while next_game == 10
+       next_game = janken
+      end
+  end
+  else
+    next_game = false
   end
 end
 
-while next_game == 3 do
-  if hoi_lose == 1
-    next_game = janken
+while next_game == 30 do
+  puts "あっち向いてホイをします"
+  puts "あなたは顔を向けます"
+  if hoi_lose == 201
+    next_game  = janken
+     if while next_game == 10
+       next_game = janken
+      end
+  end
+  else
+    next_game = false
   end
 end
 
-
-case next_game
-  when 2
-    puts "あっち向いてホイをします"
-    puts "あなたは指を指します" 
-    if hoi_win == 1
-      next_game
-    end
-
-  when 3
-    puts "あっち向いてホイをします"
-    puts "あなたは顔を向けます"
-    if hoi_lose == 1
-    next_game
-    end
-end
