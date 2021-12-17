53528bf7a45d284fc18fa1787f330dc245c6ffb5
shogoyamaoe <shogo.1227@mineo.jp>
2021-12-17 10:07:27 +0900

    じゃんけん課題

--- janken.rb
+++ janken.rb
@@ -1,6 +1,10 @@
+puts "じゃんけんを始めます" 
+puts "最初はぐー。。。じゃんけん。。。。"
+
 def janken 
   puts "[0]:グー\n[1]:チョキ\n[2]:パー"
   player_hand = gets.to_i
+  # program_hand = 0
   program_hand = rand(3)
   
   jankens = ["グー", "チョキ", "パー"]
@@ -11,7 +15,7 @@ def janken
     return 1
   elsif (player_hand == 0 && program_hand == 1) || (player_hand == 1 && program_hand == 2) || (player_hand == 2 && program_hand == 0)
     puts "じゃんけんはあなたの勝ちです"
-    return 2  
+    return 2
   elsif (player_hand == 0 && program_hand == 2) || (player_hand == 1 && program_hand == 0) || (player_hand == 2 && program_hand == 1)
     puts "じゃんけんはあなたの負けです"
     return 3
@@ -25,13 +29,19 @@ end
 def hoi_win
   puts "[0]:左\n[1]:上\n[2]:右\n[3]:下"
   player = gets.to_i
+  # program = 0
   program = rand(4)
-
+  
   hoi_win = ["左", "上", "右","下"]
   puts "あなた:#{hoi_win[player]}, わたしの方向:#{hoi_win[program]}"
     
   if player == program
     puts "あなたの勝ち"
+  
+  elsif player != program
+    puts "もう一度じゃんけんしよう！！"
+    return 1
+
   else
     puts "じゃんけんから始めましょう"
   end
@@ -40,35 +50,54 @@ end
 def hoi_lose
   puts "[0]:左\n[1]:上\n[2]:右\n[3]:下"
   player = gets.to_i
+  # program = 0
   program = rand(4)
+
 
   hoi_lose = ["左", "上", "右","下"]
   puts "あなた:#{hoi_lose[player]}, わたしの方向:#{hoi_lose[program]}"
     
   if player == program
     puts "あなたの負け"
-  else
+  elsif player != program
+    puts "もう一度じゃんけんしよう！！"
+    return 1
+  else 
     puts "じゃんけんから始めましょう"
   end
 end
 
-next_game = 1
-  puts "最初はグー、じゃんけん..."
+next_game = janken
   
-while  next_game <= 1 do
+while next_game == 1 do
   next_game = janken
 end
+
+while next_game == 2 do
+  if hoi_win == 1
+    next_game = janken
+  end
+end
+
+while next_game == 3 do
+  if hoi_lose == 1
+    next_game = janken
+  end
+end
+
 
 case next_game
   when 2
     puts "あっち向いてホイをします"
     puts "あなたは指を指します" 
-    next_game = hoi_win
-    
+    if hoi_win == 1
+      next_game
+    end
+
   when 3
     puts "あっち向いてホイをします"
     puts "あなたは顔を向けます"
-    next_game = hoi_lose
-  else
-    puts "どのパターンに当てはまりません"
+    if hoi_lose == 1
+    next_game
+    end
 end
